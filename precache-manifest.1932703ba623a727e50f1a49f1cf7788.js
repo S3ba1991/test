81378c5d4d7a74eb5732ea7a09b5f1d29f05abde
S3ba1991 <s3babdg@gmail.com>
2020-01-10 15:03:41 +0100

    Updates

---
+++ precache-manifest.1932703ba623a727e50f1a49f1cf7788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845214f41886c121761c87d1c69ce673",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "afb58552123b83619717",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1e76a8c553c22fc2e2bc",
+    "url": "/test/static/js/2.5b7a7b0c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.5b7a7b0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afb58552123b83619717",
+    "url": "/test/static/js/main.e3b22ce1.chunk.js"
+  },
+  {
+    "revision": "b1aacd4532bac6a309fe",
+    "url": "/test/static/js/runtime-main.89d58a87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
